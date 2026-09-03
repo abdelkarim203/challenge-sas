465a947d4a52f27d82b07e25fd03657451d8c3ee
abdelkarim203 <karimmarzouk92@gmail.com>
2026-09-03 10:54:02 +0100

    challenge5

---
+++ boucle/challenge1.js
@@ -0,0 +1,9 @@
+
+
+const prompt = require('prompt-sync')();
+let number = +prompt('enter your number : ')
+
+for(participant = 1 ; participant <= number ; participant++){
+    console.log("participant " + participant);
+}
+    
