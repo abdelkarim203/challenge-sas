1b551513b53a9d1e60845fff816b18ca041649b3
abdelkarim203 <karimmarzouk92@gmail.com>
2026-09-05 12:11:25 +0100

    challenge3

--- boucle/challenge3.js
+++ boucle/challenge3.js
@@ -1,4 +1,10 @@
 
+<<<<<<< HEAD
+
+const prompt = require('prompt-sync')();
+let number = +Prompt('enter your number : ')
+
+=======
 const prompt = require('prompt-sync')();
 let N = +prompt('enter your N : ');
 
@@ -8,3 +14,4 @@ for(let i = 1 ; i <= N ; i++){
     
 }
 console.log(somme)
+>>>>>>> b47b95fa662ebdc3bf5640edf7685de503add5ac
